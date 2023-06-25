7b28fe6dbc9961cfd6d38853688001acece08ef9
Nurullah Arslan <84245871+nurullah4rslan@users.noreply.github.com>
2023-06-25 20:29:12 +0300

    bug fixed

--- Src/Configs/routes.js
+++ Src/Configs/routes.js
@@ -1,18 +1,18 @@
-import {ACADEMIC_LIST} from '../Services/networkPaths';
-
 export default {
   LOGIN: 'Giriş Sayfası',
   HOME: 'Gelen Mesajlar',
   GROUPS: 'Ders Sohbetleri',
   UNICS: 'özel Mesajlar',
-  TOPNAVIGATOR: 'Fırat Üniversitesi İletisim Platformu',
+  TOPNAVIGATOR: 'Fırat İletisim Platformu',
   DRAWERNAVIGATOR: 'DrawerBar',
   COMMON: 'Genel Duyurular',
-  SECTION: 'Bolum Duyuruları',
+  SECTION: 'Bölüm Duyuruları',
   EXIT: 'Çıkış',
   MESSAGE: 'Mesajlar',
   ALL: 'Gelen Mesajlar',
   ACADEMIC_LIST: 'Akademisyen Listesi',
   GROUP_PROFILE: 'Grup Profili',
   USER_PROFILE: 'Kullanıcı Profilleri',
+  SECTION_DETAIL: 'Bölüm Duyuruları Detay',
+  COMMON_DETAIL: 'Genel Duyurular Detay',
 };
