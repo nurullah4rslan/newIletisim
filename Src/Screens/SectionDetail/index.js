82874a8dfa0f96cffcc29ab3526ae9536eb050c8
Nurullah Arslan <84245871+nurullah4rslan@users.noreply.github.com>
2023-06-25 20:26:02 +0300

    case 34

---
+++ Src/Screens/SectionDetail/index.js
@@ -0,0 +1,123 @@
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import styles from './styles';
+import {useSelector} from 'react-redux';
+import * as cheerio from 'cheerio-without-node-native';
+import Header from '../../Components/header';
+import {ScrollView, TouchableHighlight} from 'react-native-gesture-handler';
+export default function SectionDetail({navigation, route}) {
+  console.log('route', route.params.link);
+  const state = useSelector(state => state);
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
+  const [mainNumbers, setMainNumbers] = useState();
+  const [mainNumber, setMainNumber] = useState([]);
+  const urlWorldOMeter = route.params.link;
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  async function fetchData() {
+    fetch(urlWorldOMeter)
+      .then(response => response.text())
+      .then(data => {
+        const html = cheerio.load(data);
+        setMainNumbers(getMainNumbers(html));
+        console.log('mainNumbers', mainNumbers);
+      })
+      .catch(err => console.warn('Something went wrong.', err));
+  }
+  const getMainNumbers = $ => {
+    const mainValues = $('.new-section-detail-title ');
+    const mainValues1 = $('.new-section-detail-explanation ');
+    const mainValues2 = $('.new-section-detail-date ');
+
+    const title = mainValues.eq(0).text().trim();
+    const body = mainValues1.eq(0).text().trim();
+    const date = mainValues2.eq(0).text().trim();
+    const result = {
+      title: title.replace(/\s+/g, ' '),
+      body: body.replace(/\s+/g, ' '),
+      date: date.replace(/\s+/g, ' '),
+    };
+    return result;
+  };
+  console.log('mainNumbers', mainNumbers);
+  console.log('mainNumber-------', mainNumber);
+  return (
+    <View
+      style={[
+        styles.Container,
+        {backgroundColor: state.personType == 0 ? '#E90348' : '#01AAC1'},
+      ]}>
+      <Header
+        head_title={'Duyuru Detayı'}
+        onpress={() => navigation.goBack()}
+      />
+      <View style={{height: 50, justifyContent: 'center'}}>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'white',
+          flex: 1,
+          width: '100%',
+          borderTopRightRadius: 40,
+          borderTopLeftRadius: 40,
+          alignItems: 'center',
+        }}>
+        <ScrollView
+          style={{
+            marginVertical: 30,
+          }}>
+          <View style={{alignItems: 'center'}}>
+            <Image
+              source={{uri: route.params.photo}}
+              style={{
+                width: windowWidth * 0.5,
+                height: windowWidth * 0.5,
+                marginBottom: 20,
+              }}
+            />
+          </View>
+          <View
+            style={{
+              width: windowWidth,
+              height: 1,
+              backgroundColor: 'black',
+            }}></View>
+          <View>
+            <Text
+              style={{
+                fontSize: 17,
+                fontWeight: '700',
+                marginHorizontal: 20,
+                textAlign: 'center',
+                marginVertical: 20,
+                color: 'black',
+              }}>
+              {mainNumbers?.title}
+            </Text>
+            <Text
+              style={{
+                fontSize: 13,
+                marginHorizontal: 20,
+                color: 'black',
+              }}>
+              {mainNumbers?.body}
+            </Text>
+            <View style={{alignItems: 'flex-end', margin: 20}}>
+              <Text style={{color: 'grey'}}>{mainNumbers?.date}</Text>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    </View>
+  );
+}
